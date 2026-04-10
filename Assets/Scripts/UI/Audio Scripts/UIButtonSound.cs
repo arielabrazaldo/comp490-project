e403906c55354195c5e230ca71ce83cf98cb5b84
Ariel Abrazaldo <arielabrazaldo1214@yahoo.com>
2026-04-10 01:42:31 -0700

    main menu sound fx added

--- Assets/Scripts/UI/Audio Scripts/UIButtonSound.cs
+++ Assets/Scripts/UI/Audio Scripts/UIButtonSound.cs
@@ -4,12 +4,17 @@ public class UIButtonSound : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip clickClip;
+    [SerializeField] private AudioClip backClip;
 
     public void PlayClick()
     {
         if (audioSource != null && clickClip != null)
-        {
             audioSource.PlayOneShot(clickClip);
-        }
+    }
+
+    public void PlayBack()
+    {
+        if (audioSource != null && backClip != null)
+            audioSource.PlayOneShot(backClip);
     }
 }
