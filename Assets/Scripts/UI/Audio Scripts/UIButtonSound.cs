dc62396620a615575975c3e47173272a9394e90f
Ariel Abrazaldo <arielabrazaldo1214@yahoo.com>
2026-05-01 15:08:37 -0700

    wired sounds to buttons in board editor

--- Assets/Scripts/UI/Audio Scripts/UIButtonSound.cs
+++ Assets/Scripts/UI/Audio Scripts/UIButtonSound.cs
@@ -7,28 +7,63 @@ public class UIButtonSound : MonoBehaviour
     [SerializeField] private AudioClip backClip;
     [SerializeField] private AudioClip customGridClip;
     [SerializeField] private AudioClip startGameClip;
+    [SerializeField] private AudioClip resetClip;
+    [SerializeField] private AudioClip boardEditorClip;
+    [SerializeField] private AudioClip ruleEditorClip;
 
+    // button press sound
     public void PlayClick()
     {
         if (audioSource != null && clickClip != null)
             audioSource.PlayOneShot(clickClip);
     }
 
+    // back button sound
     public void PlayBack()
     {
         if (audioSource != null && backClip != null)
             audioSource.PlayOneShot(backClip);
     }
 
+    // open custom grid sound
     public void PlayCustomGrid()
     {
         if (audioSource != null && customGridClip != null)
             audioSource.PlayOneShot(customGridClip);
     }
 
+    // start button sound
     public void startGame()
     {
         if (audioSource != null && startGameClip != null)
             audioSource.PlayOneShot(startGameClip);
     }
+
+    /*
+     * 
+     *  Board Editor/Rule Editor Sounds
+     *  
+     */
+
+    // reset button sound
+    public void Reset()
+    {
+        if (audioSource != null && resetClip != null)
+            audioSource.PlayOneShot(resetClip);
+    }
+
+    // open board editor sound
+    public void BoardEditor()
+    {
+        if (audioSource != null && boardEditorClip != null)
+            audioSource.PlayOneShot(boardEditorClip);
+    }
+
+    // open rule editor sound
+    public void RuleEditor()
+    {
+        if (audioSource != null && ruleEditorClip != null)
+            audioSource.PlayOneShot(ruleEditorClip);
+    }
+
 }
